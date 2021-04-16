cd8868eaae854a6ffd26670153e03758c6d85db2
jesuscirino <jesuscirino@gmail.com>
2021-04-16 17:28:51 -0500

    bot

--- bot.js
+++ bot.js
@@ -1,21 +1,60 @@
 const { Client } = require("whatsapp-web.js");
+const fs = require("fs");
 const qrcode = require("qrcode-terminal");
-const client = new Client();
+const { Console } = require("console");
+const chalk = require("chalk");
+const ora = require("ora");
 
-client.on("qr", (qr) => {
-  // Generate and scan this code with your phone
-  console.log("QR RECEIVED", qr);
-  qrcode.generate(qr, { small: true });
-});
+const SESION = "./session.json";
+let client;
+let sesion_datos;
 
-client.on("ready", () => {
-  console.log("Client is ready!");
-});
+const sesionAbierta = () => {
+  // Si archivo json con credes
+  const spinner = ora(
+    `Cargando ${chalk.yellow("verificando sesión wa.me...")}`
+  );
+  sesion_datos = require(SESION);
+  spinner.start();
+  client = new Client({ session: sesion_datos });
+  client.on("ready", () => {
+    console.log("cliente listo!!");
+    spinner.stop();
+    client.sendMessage("+527641195729@c.us", "Hola");
+    //escuchaMensaje();
+  });
+  client.on("auth_failure", () => {
+    spinner.stop();
+    console.log("Error de Auth, borra session.json!");
+  });
+  client.initialize();
+  escuchaMensaje();
+};
 
-client.on("message", (msg) => {
-  if (msg.body == "!ping") {
-    msg.reply("pong");
-  }
-});
-
-client.initialize();
+// Para el QR
+const sesionCerrada = () => {
+  console.log("No hay sesión");
+  client = new Client();
+  client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
+  client.on("authenticated", (session) => {
+    //guardar credenciales
+    fs.writeFile(SESION, JSON.stringify(session), function (err) {
+      if (err) {
+        Console.log(err);
+      }
+    });
+  });
+  client.initialize();
+};
+const escuchaMensaje = () => {
+  client.on("message", (msg) => {
+    const { from, to, body } = msg;
+    console.log(msg);
+  });
+};
+const mandaMensaje = (to, msg) => {
+  client.sendMessage(to, msg);
+};
+fs.existsSync(SESION) ? sesionAbierta() : sesionCerrada();
