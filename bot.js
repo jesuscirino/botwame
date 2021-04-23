4b30897e09f3ee54ae80e0f52e3f8a801c6f6d38
jesuscirino <jesuscirino@gmail.com>
2021-04-23 18:35:49 -0500

    excel con sin o incorrecto nobre de imag

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const qrcode = require("qrcode-terminal");
-const { Client } = require("whatsapp-web.js");
+const { Client, MessageMedia } = require("whatsapp-web.js");
 const Excel = require("exceljs");
 
 const RUTA_EXCEL = "../../botWB/contactos.xlsx";
-const COL_NUMS = 1;
+const COL_NUMS = 2;
 const CEL_MENSAJE = "A2";
 const CEL_IMAGEN = "B2";
 const LIBRO = new Excel.Workbook();
@@ -12,13 +12,18 @@ const LIBRO = new Excel.Workbook();
 const SESION_JSON = "./session.json";
 //var client;
 
-const leerLibro = async (ruta) => {
+const leerNumeros = async (ruta) => {
+  let nums = [];
   await LIBRO.xlsx.readFile(ruta);
   console.log(`Archivo de excel ${RUTA_EXCEL} modificado:${LIBRO.modified}`);
   const hoja1 = LIBRO.getWorksheet("Hoja1");
   const hoja2 = LIBRO.getWorksheet("Hoja2");
-  console.log(hoja1.getColumn(COL_NUMS).values);
-  console.log(hoja2.getCell("A2").value);
+  nums = hoja1
+    .getColumn(COL_NUMS)
+    .values.filter((value) => typeof value === "number");
+  nums.unshift(hoja2.getCell(CEL_IMAGEN).value);
+  nums.unshift(hoja2.getCell(CEL_MENSAJE).value);
+  return nums;
 };
 
 const conSesGuardada = async () => {
@@ -28,6 +33,7 @@ const conSesGuardada = async () => {
   let client = new Client({
     session: sessionData,
   });
+  client.initialize();
 
   client.on("auth_failure", () => {
     console.log(
@@ -43,6 +49,7 @@ const sinSesGuardada = async () => {
   client.on("qr", (qr) => {
     qrcode.generate(qr, { small: true });
   });
+  client.initialize();
 
   client.on("auth_failure", () => {
     console.log("** Error de autentificacion vuelve a generar el QRCODE **");
@@ -64,13 +71,44 @@ const sinSesGuardada = async () => {
 };
 
 (async () => {
-  await leerLibro(RUTA_EXCEL);
+  let datos = await leerNumeros(RUTA_EXCEL);
   let client = fs.existsSync(SESION_JSON)
     ? await conSesGuardada()
     : await sinSesGuardada();
-  await client.initialize();
-  console.log(client.info);
-  client.on("ready", () => {
-    console.log(`WhatsApp Listo ${String(client.info.wid)}`);
+  //await client.initialize();
+  client.on("ready", async () => {
+    console.log(`WhatsApp Listo ...`);
+    //console.log(client.info.wid);
+    //console.log(datos);
+    let msg = datos.shift();
+    let img = datos.shift();
+    if (img === null) img = "";
+    let media = undefined;
+    try {
+      media =
+        img.includes(".jpg") | img.includes(".png")
+          ? MessageMedia.fromFilePath(`./imags/${img}`)
+          : null;
+    } catch (error) {
+      console.error(`no se encuentra el archivo: ${img} !!!`);
+      process.exit(0);
+    }
+    datos.forEach(async (num) => {
+      try {
+        if (media === null) {
+          await client
+            .sendMessage(`521${num}@c.us`, msg)
+            .then(console.log(`mensaje solo texto enviado a ${num}`));
+        } else {
+          await client
+            .sendMessage(`521${num}@c.us`, media, { caption: msg })
+            .then(console.log(`mensaje con imagen enviado a ${num}`));
+        }
+      } catch (error) {
+        console.error(new Error(`error con ${num}`));
+      }
+    });
   });
+  //console.log(client);
+  //client.sendMessage("5215612047623@c.us", "hola");
 })();
